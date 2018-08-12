07bad05f936ed606c19da7e3fc28de2848a6593f
Eric Arbour <arboureric@gmail.com>
2018-08-12 17:19:48 -0400

    initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import SignalConnection from '../SignalConnection/SignalConnection';
+
+class App extends React.Component {
+  public render() {
+    return <SignalConnection />;
+  }
+}
+
+export default App;
---
+++ src/SignalConnection/SignalConnection.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+
+const connection = new HubConnectionBuilder()
+  .withUrl('http://192.168.1.78:5000/chatHub')
+  .build();
+
+interface IState {
+  playerCount: number;
+}
+
+class SignalConnection extends React.Component<{}, IState> {
+  public state: IState = {
+    playerCount: 1
+  };
+
+  public componentDidMount() {
+    connection.start().catch(err => console.error(err.toString()));
+
+    connection.on('PlayerCount', playerCount => {
+      this.setState({ playerCount });
+    });
+  }
+
+  public render() {
+    return <div>Number of players: {this.state.playerCount}</div>;
+  }
+}
+
+export default SignalConnection;
