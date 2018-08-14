8590bfb7435376f8446694c4ba4b90f47f55c0be
Eric Arbour <arboureric@gmail.com>
2018-08-13 22:18:19 -0400

    create and listen for group creations

--- src/SignalConnection/SignalConnection.tsx
+++ src/SignalConnection/SignalConnection.tsx
@@ -2,32 +2,63 @@ import * as React from 'react';
 import { HubConnectionBuilder } from '@aspnet/signalr';
 
 const connection = new HubConnectionBuilder()
-  .withUrl('http://192.168.1.78:5000/chatHub')
+  .withUrl('http://192.168.1.78:5000/pokerHub')
   .build();
 
 interface IState {
+  groupName: string;
   playerCount: number;
 }
 
 class SignalConnection extends React.Component<{}, IState> {
   public state: IState = {
+    groupName: '',
     playerCount: 1
   };
+
+  constructor(props: any) {
+    super(props);
+
+    this.createGroup = this.createGroup.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
   public componentDidMount() {
     connection.start().catch(err => console.error(err.toString()));
 
-    connection.on('PlayerCount', playerCount => {
-      this.setState({ playerCount });
-    });
-
-    connection.on('GameReady', () => {
-      console.log('we ready we ready');
+    connection.on('GroupCreated', groups => {
+      console.log(groups);
     });
   }
 
+  public createGroup() {
+    const { groupName } = this.state;
+    connection
+      .invoke('CreateGroup', groupName)
+      .catch(err => console.error(err.toString()));
+    this.setState({ groupName: '' });
+  }
+
+  public handleChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ groupName: event.currentTarget.value });
+  }
+
   public render() {
-    return <div>Number of players: {this.state.playerCount}</div>;
+    console.log(this.state.groupName);
+    return (
+      <div>
+        <label>
+          Table Name
+          <input value={this.state.groupName} onChange={this.handleChange} />
+        </label>
+        <button
+          disabled={this.state.groupName.length === 0}
+          onClick={this.createGroup}
+        >
+          Create Group
+        </button>
+      </div>
+    );
   }
 }
 
