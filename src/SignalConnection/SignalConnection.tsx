52259c5c9a2a4defaec529862e539759bed56ff3
Eric Arbour <arboureric@gmail.com>
2018-09-01 23:37:32 -0400

    return to microservice architecture

---
+++ src/SignalConnection/SignalConnection.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+import Table from '../Table/Table';
+import { ITable } from 'shared/src/types/interfaces';
+
+const connection = new HubConnectionBuilder()
+  .withUrl('http://192.168.1.78:5000/pokerHub')
+  .build();
+
+interface IState {
+  connected: boolean;
+  error: string;
+  tableInfo: ITable | null;
+  tableName: string;
+}
+
+class SignalConnection extends React.Component<{}, IState> {
+  public state: IState = {
+    connected: false,
+    error: '',
+    tableInfo: null,
+    tableName: ''
+  };
+
+  constructor(props: any) {
+    super(props);
+
+    this.createTable = this.createTable.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  public componentDidMount() {
+    connection
+      .start()
+      .then(() => this.setState({ connected: true }))
+      .catch(err => this.setState({ error: err.toString() }));
+
+    connection.on('TableCreated', tableInfo => {
+      this.setState({ tableInfo });
+    });
+
+    connection.on('TableUpdated', tableInfo => {
+      this.setState({ tableInfo });
+    });
+  }
+
+  public createTable() {
+    const { tableName } = this.state;
+    connection
+      .invoke('CreateTable', tableName)
+      .catch(err => this.setState({ error: err.toString() }));
+  }
+
+  public handleChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ tableName: event.currentTarget.value });
+  }
+
+  public render() {
+    console.log();
+    if (!this.state.connected) {
+      return <div>Loading...</div>;
+    }
+    if (this.state.tableInfo) {
+      return <Table tableInfo={this.state.tableInfo} />;
+    }
+    return (
+      <div>
+        <label>
+          Table Name
+          <input value={this.state.tableName} onChange={this.handleChange} />
+        </label>
+        <button
+          disabled={this.state.tableName.length === 0}
+          onClick={this.createTable}
+        >
+          Create Group
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SignalConnection;
